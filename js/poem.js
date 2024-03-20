cd76fdd7835f70bc58fa76ac3767e16cb9a0ac0c
CoolCoolOne <Aleksey-1998@yandex.ru>
2024-03-20 14:48:29 +0300

    change poem page/ better  theme changes script

--- js/poem.js
+++ js/poem.js
@@ -1,7 +1,25 @@
-document.querySelector('.themes').addEventListener('change',(event)=>{
-    if (event.target.nodeName === 'INPUT') {
-        // console.log(bbhbjhbh);
-        document.documentElement.classList.remove('dark','light');
-        document.documentElement.classList.add(event.target.value);
-    }
-});
+// document.querySelector('.themes').addEventListener('change',(event)=>{
+//     if (event.target.nodeName === 'INPUT') {
+//         // console.log(bbhbjhbh);
+//         document.documentElement.classList.remove('dark','light');
+//         document.documentElement.classList.add(event.target.value);
+//     }
+// });
+
+console.log('hello!!')
+const lightT = document.querySelector('#light');
+const DarkT = document.querySelector('#dark');
+const htmldiv = document.querySelector('#htmlId');
+
+
+lightT.addEventListener('click',function(){
+    console.log('click!!!');
+    htmldiv.classList.remove('dark');
+    htmldiv.classList.add('light');
+})
+
+DarkT.addEventListener('click',function(){
+    console.log('click!!!');
+    htmldiv.classList.remove('light');
+    htmldiv.classList.add('dark');
+})
